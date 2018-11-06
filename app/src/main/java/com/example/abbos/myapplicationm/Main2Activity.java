ff9266369dd2013a53a7cefcc1e22be87af514a4
Abbosqwe <saidovabbos1995@gmail.com>
2018-11-06 21:22:56 +0500

    ushade

--- app/src/main/java/com/example/abbos/myapplicationm/Main2Activity.java
+++ app/src/main/java/com/example/abbos/myapplicationm/Main2Activity.java
@@ -1,5 +1,6 @@
 package com.example.abbos.myapplicationm;
 
+import android.app.Activity;
 import android.app.SearchManager;
 import android.app.SearchableInfo;
 import android.content.Context;
@@ -17,16 +18,45 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+
 
 public class Main2Activity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    int[] IMAGES ={R.drawable.imagelogo};
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ListView listView =findViewById(R.id.listview);
+
+        CustomAdapter customAdapter=new CustomAdapter();
+
+        listView.setAdapter(customAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.i("Clicked","Clicked="+position);
+
+                Intent myIntent = new Intent(Main2Activity.this, ItemClicked.class);
+                myIntent.putExtra("key", position); //Optional parameters
+                Main2Activity.this.startActivity(myIntent);
+            }
+        });
+
 
         handleIntent(getIntent());
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -90,7 +120,8 @@ public class Main2Activity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
         }
 
         return super.onOptionsItemSelected(item);
@@ -103,13 +134,14 @@ public class Main2Activity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_search) {
-            // Handle the camera action
+            toolbar.setTitle("All Movies");
         } else if (id == R.id.nav_top) {
-
+            toolbar.setTitle("Top movies of All Time");
         } else if (id == R.id.nav_highest) {
-
+            toolbar.setTitle("Highest grossing films ");
         } else if (id == R.id.nav_settings) {
-
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
         }
         /*else if (id == R.id.nav_sign) { }*/
 
@@ -117,4 +149,40 @@ public class Main2Activity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+class CustomAdapter extends BaseAdapter{
+
+    @Override
+    public int getCount() {
+        return 10;
+    }
+
+    @Override
+    public Object getItem(int position) {
+
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        convertView =getLayoutInflater().inflate(R.layout.cutomlayout,null);
+        ImageView imageView=convertView.findViewById(R.id.imageView3);
+
+        TextView textView =convertView.findViewById(R.id.textView2);
+        TextView textView2 =convertView.findViewById(R.id.textView3);
+
+         imageView.setImageResource(IMAGES[0]);
+         textView.setText("UShade"+position);
+        textView2.setText("UShade2=");
+
+        return convertView;
+    }
+}
+
 }
